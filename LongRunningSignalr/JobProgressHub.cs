e5468b9965ccc1c2dcea32c974827426223f4289
Jerrie Pelser <jerrie@jerriepelser.com>
2018-06-28 20:07:28 +0700

    Remove code to remove from connection from group

--- LongRunningSignalr/JobProgressHub.cs
+++ LongRunningSignalr/JobProgressHub.cs
@@ -8,24 +8,7 @@ namespace LongRunningSignalr
     {
         public async Task AssociateJob(string jobId)
         {
-            Context.Items.Add("JobId", jobId);
-            
             await Groups.AddToGroupAsync(Context.ConnectionId, jobId);
-        }
-        
-        public override async Task OnConnectedAsync()
-        {
-            await base.OnConnectedAsync();
-        }
-
-        public override async Task OnDisconnectedAsync(Exception exception)
-        {
-            if (Context.Items["JobId"] is string jobId)
-            {
-                await Groups.RemoveFromGroupAsync(Context.ConnectionId, jobId);
-            }
-            
-            await base.OnDisconnectedAsync(exception);
         }
     }
 }

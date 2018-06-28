0a11688e360afed8984db542967fa1e65ec18bf2
Jerrie Pelser <jerrie@jerriepelser.com>
2018-06-28 13:30:19 +0700

    Initial commit

---
+++ LongRunningSignalr/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Coravel.Queuing.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using LongRunningSignalr.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LongRunningSignalr.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IQueue _queue;
+        private readonly IHubContext<JobProgressHub> _hubContext;
+
+        public HomeController(IQueue queue, IHubContext<JobProgressHub> hubContext)
+        {
+            _queue = queue;
+            _hubContext = hubContext;
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult StartProgress()
+        {
+            string jobId = Guid.NewGuid().ToString("N");
+            _queue.QueueAsyncTask(() => PerformBackgroundJob(jobId));
+
+            return RedirectToAction("Progress", new {jobId});
+        }
+
+        public IActionResult Progress(string jobId)
+        {
+            ViewBag.JobId = jobId;
+
+            return View();
+        }
+
+        private async Task PerformBackgroundJob(string jobId)
+        {
+            for (int i = 0; i <= 100; i += 5)
+            {
+                await _hubContext.Clients.Group(jobId).SendAsync("progress", i);
+
+                await Task.Delay(100);
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LongRunningSignalr/JobProgressHub.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace LongRunningSignalr
+{
+    public class JobProgressHub : Hub
+    {
+        public async Task AssociateJob(string jobId)
+        {
+            Context.Items.Add("JobId", jobId);
+            
+            await Groups.AddToGroupAsync(Context.ConnectionId, jobId);
+        }
+        
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            if (Context.Items["JobId"] is string jobId)
+            {
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, jobId);
+            }
+            
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
